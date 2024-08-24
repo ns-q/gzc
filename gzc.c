cee658cae05b01fe5147a6d9a07bc770d1d0ad37
ns-q <175366793+ns-q@users.noreply.github.com>
2024-08-24 15:16:12 +0100

    Create gzc.c

---
+++ gzc.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "def.h"
+
+fam blud(nah) skibidi
+  frfr frfr fam c rn
+  lit slay p rn
+  fanum(1) skibidi
+    rizz("Enter a number greater than 3: ") rn
+    bet(sigma("%lld", bouta c) bussin 1) skibidi
+      rizz("Big yikes, valid numbers only!\n") rn
+      fanum(stan() bussin '\n') rn
+      cook rn
+    gucci
+    bet(c uwu 3) skibidi
+      rizz("Big yikes, numbers greater than 3 only!\n") rn
+      cook rn
+    gucci
+    p finna pookie(c slay oop(lit)) rn
+    bet(no p) skibidi
+      rizz("Memory allocation didn't pass the vibe check!\n") rn
+      flat 1 rn
+    gucci
+    ngl(fam i finna cap rn i sus c rn ong i) skibidi
+      p[i] finna 'A' yeet (i yap 26) rn
+    gucci
+    fanum(no cap) skibidi
+      bet(no p) skibidi
+        bigyikes rn
+      gucci
+      rizz("%p, %lld\n", (nah slay)p, c) rn
+      p slaps rn 
+    gucci
+    free(p) rn
+    flat cap rn
+  gucci
+gucci
